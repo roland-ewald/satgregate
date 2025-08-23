4218c7e01c4db6275998d740b614bf11ca79c80f
Roland Ewald <roland.ewald@limbus-medtec.com>
2025-08-23 23:48:40 +0200

    Family structure, basic import (untested)

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 
+# satgregate
 
 This is work in progress (of a rough idea, which may or may not work). Nothing to see here.
+
+## Related
+
+- Reed, Mary. ["Algebraic structure of genetic inheritance."](https://www.ams.org/journals/bull/1997-34-02/S0273-0979-97-00712-X/S0273-0979-97-00712-X.pdf) Bulletin of the American Mathematical Society 34, no. 2 (1997): 107-130.
+
+- Lynce, Inês, and Joao Marques-Silva. ["Efficient haplotype inference with Boolean satisfiability."](https://cdn.aaai.org/AAAI/2006/AAAI06-017.pdf) In National conference on artificial intelligence (AAAI). AAAI Press, 2006.
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,58 @@
 use noodles_vcf as vcf;
-use std::io::Error;
+use std::collections::HashMap;
+use std::rc::Rc;
+use std::{cell::RefCell, io::Error};
+
+enum Genotype {
+    //HomRef, -- no gVCF support yet
+    //Unknown, -- no BED support yet
+    Het,
+    HomAlt,
+}
+
+struct Individual {
+    sample_id: String,
+    father_id: Option<String>, //RefCell<Rc<Individual>>?
+    mother_id: Option<String>,
+    affected: bool,
+}
+
+struct Family {
+    individuals: HashMap<String,Individual>, // TODO: check all sample_ids (father/mother) are present
+}
+
+
+pub fn build_family_structure_from_csv(file_path: &str) -> Result<Family, Error> {
+    let mut family_csv = csv::Reader::from_path(file_path)?;
+    let mut individuals: HashMap<String,Individual> = HashMap::new();
+    for result in family_csv.records() {
+        let record = result?;
+        let sample_id = record.get(0).unwrap().to_string();
+        let father_id = match record.get(1).unwrap().trim() {
+            "" => None,
+            s => Some(s.to_string()),
+        };
+        let mother_id = match record.get(2).unwrap().trim() {
+            "" => None,
+            s => Some(s.to_string()),
+        };
+        let affected: bool = match record.get(3).unwrap().trim().to_ascii_lowercase().as_str() {
+            "1"|"y"|"yes"|"t"|"true"|"affected" => true,
+            "0"|"n"|"no"|"f"|"false"|"unaffected" => false, 
+            _ => return Err(Error::new(std::io::ErrorKind::InvalidInput, 
+                format!("Affected status must be '1|y|yes|t|true|affected' or '0|n|no|ff|false|unaffected', found '{}'", record.get(3).unwrap()))),
+        };
+        let individual = Individual {
+            sample_id: sample_id.clone(),
+            father_id,
+            mother_id,
+            affected,
+        };
+        individuals.insert(sample_id, individual);
+    }
+    Ok(Family { individuals })
+}
+
 
 #[allow(dead_code)] // Work in progress
 fn read_vcf_file(file_path: &str) -> Result<vcf::Header, Error> {
